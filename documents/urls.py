63c7bb6d3c4be8a6a861e129acf69a5ef1111b18
Pol <52445798+poltawin@users.noreply.github.com>
2025-05-23 16:52:15 +0700

    temporary remove auto coa and msds

--- documents/urls.py
+++ documents/urls.py
@@ -8,8 +8,8 @@ path('generate/spec/', views.generate_spec_view, name='generate_spec'),
 path('generate/coa/', views.generate_coa_view, name='generate_coa'),
 path('generate/msds/', views.generate_msds_view, name='generate_msds'),
 path('generate/spec-from-db/', views.select_spec_view, name='select_spec'),
-path('generate/coa-from-db/', views.select_coa_view, name='select_coa'),
-path('generate/msds-from-db/', views.select_msds_view, name='select_msds'),
+# path('generate/coa-from-db/', views.select_coa_view, name='select_coa'),
+# path('generate/msds-from-db/', views.select_msds_view, name='select_msds'),
 
 
 
